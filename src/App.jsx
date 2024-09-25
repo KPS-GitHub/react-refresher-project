92bb6e8f7c10aa7860bf705250efbb79ce8831bd
Kennan Smith <kennan.smith1@gmail.com>
2024-09-25 12:26:52 -0400

    created first component, with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Post from "./Components/Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div id="post-box">
+        <Post title="Test Title from props" body="test body from props" />
+      </div>
+    </>
+    
+  );
 }
 
 export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post({title, body}) {
+    return (
+        <>
+            <h4>{title}</h4>
+            <p>{body}</p>
+        </>
+    )
+}
+
+export default Post;
