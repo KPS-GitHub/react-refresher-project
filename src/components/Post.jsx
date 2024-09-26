3e8278e2ab03e1109a979381528e58e05764cb4b
Kennan Smith <kennan.smith1@gmail.com>
2024-09-26 17:28:02 -0400

    added a little dynamic title functionality to the Post component; also added and implemented PropTypes to the Post component to ensure that the props passed to the component are properly typed and always included when required

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,21 @@
+import PropTypes from "prop-types";
+
+const titles = ['Title 1', 'Title 2']
+
 function Post({title, body}) {
+    const chosenTitle = Math.random() > 0.5 ? titles[0] : titles[1];
     return (
         <>
-            <h4>{title}</h4>
+            <h4>{chosenTitle}</h4>
             <p>{body}</p>
         </>
     )
 }
+
+
+Post.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+};
 
 export default Post;
