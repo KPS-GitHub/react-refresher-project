ca91a0088cc8fca503575701fb066407aaba3171
Kennan Smith <kennan.smith1@gmail.com>
2024-10-07 13:01:14 -0400

    added conditional rendering of new post content

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import Modal from "./components/Modal";
 function App() {
   const [newPostBody, setNewPostBody] = useState('New Post Body');
   const [newPostAuthor, setNewPostAuthor] = useState('New Post Author');
-  const [allPosts, setAllPosts] = useState([{author: "Post 1 Author", body: "Post 1 Body"}, {author: "Post 2 Author", body: "Post 2 Body"}, {author: "Post 3 Author", body: "Post 3 Body"}])
+  const [allPosts, setAllPosts] = useState([{ author: "Post 1 Author", body: "Post 1 Body" }, { author: "Post 2 Author", body: "Post 2 Body" }, { author: "Post 3 Author", body: "Post 3 Body" }])
+  const [isNewPostVisible, setIsNewPostVisible] = useState(true);
 
   function newPostBodyChangeHandler(event) {
     setNewPostBody(event.target.value);
@@ -21,7 +22,7 @@ function App() {
   function newPostSubmitHandler(event) {
     event.preventDefault();
     // put the new post at the beginning of the allPosts array so that it shows up first in the post box
-    setAllPosts([{author: newPostAuthor, body: newPostBody}, ...allPosts]);
+    setAllPosts([{ author: newPostAuthor, body: newPostBody }, ...allPosts]);
     // reset the new post state values
     setNewPostBody('New Post Body');
     setNewPostAuthor('New Post Author');
@@ -37,20 +38,48 @@ function App() {
       }
     });
   }
+  function toggleNewPostVisibleHandler() {
+    setIsNewPostVisible(!isNewPostVisible);
+  }
+
+  // // alternative modal toggle approach (instead of the ternary in the return statement - replace that whole thing with {newPostContent} to enable this approach)
+  let newPostContent;
+  if (isNewPostVisible) {
+    newPostContent =
+      <>
+        <Modal toggleNewPostVisibleHandler={toggleNewPostVisibleHandler}>
+          <NewPostForm onBodyChange={newPostBodyChangeHandler} onAuthorChange={newPostAuthorChangeHandler} onSubmit={newPostSubmitHandler} />
+        </Modal>
+        <div className={classes.newPostBox}>
+          <Post key='new-post' author={newPostAuthor} body={newPostBody} newPost={true} />
+        </div>
+      </>;
+  } else {
+    newPostContent = <button className={classes.newPostButton} onClick={toggleNewPostVisibleHandler}>New Post</button>
+  }
 
   return (
     <div className={classes.appWrap}>
-      <Modal>
-        <NewPostForm onBodyChange={newPostBodyChangeHandler} onAuthorChange={newPostAuthorChangeHandler} onSubmit={newPostSubmitHandler} />
-      </Modal>
-      <div className={classes.newPostBox}>
-        <Post key='new-post' author={newPostAuthor} body={newPostBody} newPost={true} />
-      </div>
+      {newPostContent}
+
+      {/* alternative approach to conditional rendering of new post content */}
+      {/* {isNewPostVisible ?
+        <>
+          <Modal toggleNewPostVisibleHandler={toggleNewPostVisibleHandler}>
+            <NewPostForm onBodyChange={newPostBodyChangeHandler} onAuthorChange={newPostAuthorChangeHandler} onSubmit={newPostSubmitHandler} />
+          </Modal>
+          <div className={classes.newPostBox}>
+            <Post key='new-post' author={newPostAuthor} body={newPostBody} newPost={true} />
+          </div>
+        </>
+        : <button onClick={toggleNewPostVisibleHandler}>New Post</button>
+      } */}
+
       <div className={classes.postBox}>
-        {allPosts.map((post,i) => <Post key={`post-${i}`} author={post.author} body={post.body} />)}
+        {allPosts.map((post, i) => <Post key={`post-${i}`} author={post.author} body={post.body} />)}
       </div>
     </div>
-    
+
   );
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import classes from "./Modal.module.css";
 
-function Modal({children}) {
+function Modal({children, toggleNewPostVisibleHandler}) {
     return <>
-        <div className={classes.backdrop} />
+        <div onClick={toggleNewPostVisibleHandler} className={classes.backdrop} />
         <dialog open className={classes.modal}>
             {children}
         </dialog>
